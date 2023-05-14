2b9af23969d5832be294f43b063f58e7e672d98f
Bruno Henrique <brunoh915@gmail.com>
2023-05-14 13:39:50 -0300

    Implementando alteração e deleção (incluindo imagens).

---
+++ tabela.js
@@ -0,0 +1,42 @@
+function insertProductTable(id, name, description, price) {
+    tabBody=document.getElementById("product_table");
+    row=document.createElement("tr");
+
+    var columnId = createTextColumn(id);
+    row.appendChild(columnId);
+
+    var columnName = createTextColumn(name);
+    row.appendChild(columnName);
+
+    var columnDescription = createTextColumn(description);
+    row.appendChild(columnDescription);
+
+    var columnPrice = createTextColumn(price);
+    row.appendChild(columnPrice);
+
+    var deleteButton = createHTMLButton("Excluir", `deleteProduct('${id}')`);
+    row.appendChild(deleteButton);
+
+    console.log(`editProduct(${id}, ${name}, ${description}, ${price})`);
+    var editButton = createHTMLButton("Editar", `editProduct('${id}', '${name}', '${description}', '${price}')`);
+    row.appendChild(editButton);
+
+    tabBody.appendChild(row);
+}
+
+function createTextColumn(valor) {
+    var cell = document.createElement("td");
+    var node=document.createTextNode(valor);
+    cell.appendChild(node);
+    return cell;
+}
+
+function createHTMLButton(name, onClick) {
+    var cell = document.createElement("td");
+    var button = document.createElement("button");
+    button.setAttribute("onClick", onClick);
+    var node=document.createTextNode(name);
+    button.appendChild(node);
+    cell.appendChild(button);
+    return cell;
+}
