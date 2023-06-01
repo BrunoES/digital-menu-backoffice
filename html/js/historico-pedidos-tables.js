6e23197a42b439cddd9bb8ff9a649ce103b9f445
Bruno Henrique <brunoh915@gmail.com>
2023-05-31 21:08:10 -0300

    Inclusão de informação da mesa em telas de pedido e melhoria no layout da tela de histórico de pedidos.

--- html/js/historico-pedidos-tables.js
+++ html/js/historico-pedidos-tables.js
@@ -1,9 +1,12 @@
-function insertPedido(checkoutId, dateHour, customerName, total) {
+function insertPedido(checkoutId, tableNumber, dateHour, customerName, total) {
     tabBody=document.getElementById("historico_pedidos_table");
     var row= createRow();
 
     var columnCheckoutId = createTextColumn(checkoutId);
     row.appendChild(columnCheckoutId);
+
+    var columnTableNumber = createTextColumn(tableNumber);
+    row.appendChild(columnTableNumber);
 
     var columnDateHour = createTextColumn(dateHour);
     row.appendChild(columnDateHour);
@@ -16,7 +19,7 @@ function insertPedido(checkoutId, dateHour, customerName, total) {
 
     // Botao que abre modal com detalhes do pedido, chama API de detalhes do pedido
     var cell = document.createElement("td");
-    cell.innerHTML = `<button type="button" class="btn btn-primary" style='background-color: ${LIGHT_BLUE_WHITE}; color: white; border-radius: 10px' data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="getPedidoById('${checkoutId}')">
+    cell.innerHTML = `<button type="button" class="btn btn-primary" style='background-color: ${LIGHT_BLUE_WHITE}; color: white; border-radius: 10px' data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="getPedidoById('${checkoutId}', '${tableNumber}')">
                         Detalhes
                       </button>`;
 
